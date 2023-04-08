822085b6d13d56f88ae65b2f3cd53155cf531370
Ethan P <eth-p+git@hidden.email>
2023-04-08 16:46:07 -0700

    dev: update commitlintrc to add `dev` type
    The `dev` type reflects the developer's working environment.

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,1 +1,18 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const conventional = require('@commitlint/config-conventional');
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				// Include the types from conventional commits.
+				...conventional.rules['type-enum'][2],
+
+				// Add custom types.
+				'dev',
+			].sort(),
+		],
+	},
+};
