313d7c54726e7c180dd49d433fb3e4c685b4f707
Ethan P <eth-p+git@hidden.email>
2023-04-12 13:19:03 -0700

    docs: add examples

---
+++ examples/simple-esmodule/something.ts
@@ -0,0 +1,9 @@
+import { apiVersion } from 'obsidian';
+
+export function createBox(title: string, contents: string): HTMLElement {
+	return createEl('div', {}, (el) => {
+		el.createDiv({ cls: 'title', text: title });
+		el.createDiv({ cls: 'version', text: apiVersion });
+		el.createDiv({ text: contents });
+	});
+}
